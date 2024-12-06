dda216c0d0b847b63d31bca0c48260f73cdbf68b
elerhels <elias.verhelst@ugent.be>
2024-12-06 12:36:20 +0100

    finish day 6

--- day_6/day6.cpp
+++ day_6/day6.cpp
@@ -1,17 +1,9 @@
-#include <bits/stdc++.h>
 #include <vector>
 #include <iostream>
 #include <sstream>
 #include <fstream>
-#include <unordered_map>
-
-class coord {
-    public:
-        coord(int, int);
-        int x;
-        int y;
-};
-coord::coord(int x, int y): x(x), y(y) {};
+#include <map>
+#include <set>
 
 class counter {
     public:
@@ -31,27 +23,15 @@ std::vector<std::string> get_lines(std::string filename) {
     return lines;
 };
 
-static std::vector<coord> visited;
-void trace_path(std::vector<std::string> lines, coord pos, coord bounds, int direction, counter& c) {
-    if (pos.x >= bounds.x || pos.y >= bounds.y || pos.x < 0 || pos.y < 0) { 
-        std::cout << "oob!!! (" << pos.x << "," << pos.y << ")" << std::endl; 
+void get_path_positions(std::vector<std::string> lines, std::pair<int, int> pos, std::pair<int, int> bounds, int direction, std::set<std::pair<int, int>> &visited) {
+    if (pos.first >= bounds.first || pos.second >= bounds.second || pos.first < 0 || pos.second < 0) {
         return; 
     }
 
-    bool already_visited = false;
-    for (auto point : visited) {
-        if (point.x == pos.x && point.y == pos.y) {
-            already_visited = true;
-        }
-    }
-    if (! already_visited) {
-        c.increment();
-    }
-
-    visited.push_back(pos);
-
-    int next_x = pos.x;
-    int next_y = pos.y;
+    visited.insert(pos);
+    
+    int next_x = pos.first;
+    int next_y = pos.second;
     if (direction == 0) {
         next_y--;
     } else if (direction == 1) {
@@ -61,15 +41,71 @@ void trace_path(std::vector<std::string> lines, coord pos, coord bounds, int dir
     } else {
         next_x--;    
     }
-
-    if (next_x < bounds.x && next_y < bounds.y && next_y >= 0 && next_x >= 0 && lines[next_y][next_x] == '#') {
-        std::cout << "turn" << std::endl;
+    
+    if (next_x < bounds.first && next_y < bounds.second && next_y >= 0 && next_x >= 0 && lines[next_y][next_x] == '#') {
         direction = (direction + 1) % 4;
-        trace_path(lines, pos, bounds, direction, c);
+        get_path_positions(lines, pos, bounds, direction, visited);
     } else {
-        std::cout << "move" << std::endl;
-        trace_path(lines, coord(next_x, next_y), bounds, direction, c);
+        get_path_positions(lines, {next_x, next_y}, bounds, direction, visited);
     }
+}
+
+bool path_loops(const std::vector<std::string> &lines, const std::pair<int, int> pos, const std::pair<int, int> &bounds, int direction, const std::pair<int, int> &start_pos, const int start_direction, bool first_move, std::map<std::pair<int, int>, std::vector<int>> &visited) {
+    if (pos.first >= bounds.first || pos.second >= bounds.second || pos.first < 0 || pos.second < 0) {
+        return false; 
+    }
+    if (pos.first == start_pos.first && pos.second == start_pos.second && direction == start_direction && ! first_move) {
+        return true;
+    }
+
+    for (auto dir : visited[pos]) {
+        if (dir == direction) {
+            return true;
+        }
+    }
+    visited[pos].push_back(direction);
+    
+    int next_x = pos.first;
+    int next_y = pos.second;
+    if (direction == 0) {
+        next_y--;
+    } else if (direction == 1) {
+        next_x++;
+    } else if (direction == 2) {
+        next_y++;
+    } else {
+        next_x--;    
+    }
+    
+    if (next_x < bounds.first && next_y < bounds.second && next_y >= 0 && next_x >= 0 && lines[next_y][next_x] == '#') {
+        direction = (direction + 1) % 4;
+        return path_loops(lines, pos, bounds, direction, start_pos, start_direction, false, visited);
+    } else {
+        return path_loops(lines, {next_x, next_y}, bounds, direction, start_pos, start_direction, false, visited);
+    }
+}
+
+int test_all_spots(std::vector<std::string> &lines, const std::pair<int, int> pos, const std::pair<int, int> &bounds, const int direction) {
+    int res = 0;
+    std::set<std::pair<int, int>> positions;
+    get_path_positions(lines, pos, bounds, direction, positions);
+    std::cout << positions.size() << std::endl;
+    int k = 0;
+    for (int i = 0; i < lines.size(); i++) {
+        for (int j = 0; j < lines[i].size(); j++) {
+            char orig = lines[i][j];
+            lines[i][j] = '#';
+            std::map<std::pair<int, int>, std::vector<int>> visited;
+            bool a = path_loops(lines, pos, bounds, direction, pos, direction, true, visited);
+            lines[i][j] = orig;
+            if (a) {
+                res++;
+            }
+            k++;
+            std::cout << k << "/" << (lines.size() * lines[i].size()) << std::endl;
+        }
+    }
+    return res;
 }
 
 int main(int argc, char** argv) {
@@ -77,7 +113,6 @@ int main(int argc, char** argv) {
 
     int res = 0;
 
-    counter count = counter();
     for (int i = 0; i < file.size(); i++) {
         for (int j = 0; j < file[0].size(); j++) {
             int direction = -1;
@@ -95,13 +130,11 @@ int main(int argc, char** argv) {
             }
 
             if (direction > -1) {
-                trace_path(file, coord(j, i), coord(file[0].size(), file.size()) , direction, count);
+                res = test_all_spots(file, {j, i}, {file[0].size(), file.size()}, direction);
             }
         }
     }
 
-    res = count.n;
-
-    std::cout << res << std::endl;
+    std::cout << "res: " << res << std::endl;
     return 0;
 }
